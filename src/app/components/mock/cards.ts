3c0be33101e6885028977b1a094c2c85a93b3a3a
sxj105207 <sxj105207@alibaba-inc.com>
2018-05-27 08:50:11 +0800

    feat: mock data

--- src/app/components/mock/cards.ts
+++ src/app/components/mock/cards.ts
@@ -1,2 +1,12 @@
-export class Cards {
-}
+import { Card } from '../model/card';
+
+export const Cards: Card[] = [
+	{ id: 1, ownerAddr: '0x0', candidates: [{id: 11, name: 'Monika'}], status: 'open', bounty: 1000, cost: 50, title: 'Develop', logo: 'https://www.baidu.com/img/bd_logo1.png', company: 'baidu', desc: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
+	{ id: 2, ownerAddr: '0x0', candidates: [{id: 12, name: 'Monika'}], status: 'open', bounty: 1000, cost: 50, title: 'Develop', logo: 'https://www.baidu.com/img/bd_logo1.png', company: 'baidu', desc: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
+	{ id: 4, ownerAddr: '0x0', candidates: [{id: 13, name: 'Monika'}], status: 'closed', bounty: 1000, cost: 50, title: 'Develop', logo: 'https://www.baidu.com/img/bd_logo1.png', company: 'baidu', desc: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
+	{ id: 5, ownerAddr: '0x0', candidates: [{id: 14, name: 'Monika'}], status: 'open', bounty: 1000, cost: 50, title: 'Develop', logo: 'https://www.baidu.com/img/bd_logo1.png', company: 'baidu', desc: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
+	{ id: 6, ownerAddr: '0x0', candidates: [{id: 15, name: 'Monika'}], status: 'closed', bounty: 1000, cost: 50, title: 'Develop', logo: 'https://www.baidu.com/img/bd_logo1.png', company: 'baidu', desc: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
+	{ id: 3, ownerAddr: '0x0', candidates: [{id: 16, name: 'Monika'}], status: 'open', bounty: 1000, cost: 50, title: 'Develop', logo: 'https://www.baidu.com/img/bd_logo1.png', company: 'baidu', desc: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
+	{ id: 7, ownerAddr: '0x0', candidates: [{id: 17, name: 'Monika'}], status: 'open', bounty: 1000, cost: 50, title: 'Develop', logo: 'https://www.baidu.com/img/bd_logo1.png', company: 'baidu', desc: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
+	{ id: 8, ownerAddr: '0x0', candidates: [{id: 18, name: 'Monika'}], status: 'canceled', bounty: 1000, cost: 50, title: 'Develop', logo: 'https://www.baidu.com/img/bd_logo1.png', company: 'baidu', desc: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
+];
