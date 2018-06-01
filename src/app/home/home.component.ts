c9a1cd6e733dac60e3ade8cd9ffc37aa7bbf8bf8
thetadotonline <thetadotonline@gmail.com>
2018-06-01 23:34:31 +0800

    feat: details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,36 +10,14 @@ import { ContractsService } from '../services/contracts/contracts.service';
 
 export class HomeComponent implements OnInit, AfterViewInit {
 
-  // Current user?
   currentUser: any = JSON.parse(localStorage.getItem('credentials'));
-
-  // Posts
-  posts = [
-    { postId: 0, status: 'Open', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 1, status: 'Open', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 2, status: 'Open', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 3, status: 'Closed', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 4, status: 'Closed', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 5, status: 'Closed', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 6, status: 'Closed', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 7, status: 'Cancelled', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 8, status: 'Cancelled', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 9, status: 'Cancelled', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' },
-    { postId: 10, status: 'Cancelled', title: 'Developer', ownerAddress: '0x0', ownerName: 'CanYa', bounty: 1000, cost: 50, honeypot: 1000, details: 'Full-stack solidity developer needed. Familiar with Angular, Firebase, Solidity, Truffle.' }
-  ];
-
-  // Flags
-  loading = true;
 
   constructor(private router: Router,
     private activatedRoute: ActivatedRoute,
     private cs: ContractsService) {
   }
+  ngOnInit() {
 
-  async ngOnInit() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 2000);
   }
 
   ngAfterViewInit() {
