5d82fc9e21fe32afd3a1460f5363b0e31aedc36b
thetadotonline <thetadotonline@gmail.com>
2018-07-12 11:35:29 +0800

    feat: deploy args

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  URL: {
+    changeCurrency: 'https://min-api.cryptocompare.com/data/pricemulti'
+  },
+  firebase: {
+    apiKey: "AIzaSyCxRQdQyKjdTyMbHt1F4gl7V2EGwqgekJU",
+    authDomain: "canseek-dev.firebaseapp.com",
+    databaseURL: "https://canseek-dev.firebaseio.com",
+    projectId: "canseek-dev",
+    storageBucket: "",
+    messagingSenderId: "694155772067"
+  }
 };
