3deacb5fba562b05efb1d60e9dc5a3b50db1b9c1
thetadotonline <thetadotonline@gmail.com>
2018-07-14 12:07:55 +0800

    feat: status post

--- src/app/pages/status/status.component.ts
+++ src/app/pages/status/status.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-status',
@@ -9,13 +11,17 @@ import { ActivatedRoute } from '@angular/router';
 export class StatusComponent implements OnInit {
   id: string;
   type: string;
+  host: string;
   
   constructor(
     private route: ActivatedRoute,
+    private location: Location,
+    @Inject(DOCUMENT) private document
   ) { 
     const { type, id } = this.route.snapshot.params;
     this.type = type;
     this.id = id;
+    this.host = this.document.location.origin;
   }
 
   ngOnInit() {
