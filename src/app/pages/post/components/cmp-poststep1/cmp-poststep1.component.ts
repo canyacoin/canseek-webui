552edfb070708e7652e5d84275ad9a6689df1345
thetadotonline <thetadotonline@gmail.com>
2018-07-25 21:50:49 +0800

    feat: upload file

--- src/app/pages/post/components/cmp-poststep1/cmp-poststep1.component.ts
+++ src/app/pages/post/components/cmp-poststep1/cmp-poststep1.component.ts
@@ -6,6 +6,8 @@ import {
   Validators
 } from '@angular/forms';
 import { Store } from '../../../../store';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cmp-poststep1',
@@ -44,26 +46,10 @@ export class CmpPoststep1Component implements OnInit {
     }
   }
 
-  constructor(private fb: FormBuilder) {
-  }
-
-  // tslint:disable-next-line:no-any
-  handleChange(info: any): void {
-    debugger
-    const fileList = info.fileList;
-    // 2. read from response and show file link
-    if (info.file.response) {
-      info.file.url = info.file.response.url;
-    }
-    // 3. filter successfully uploaded files according to response from server
-    this.fileList = fileList.filter(item => {
-      if (item.response) {
-        return item.response.status === 'success';
-      }
-      return true;
-    });
-  }
-
+  constructor(
+    private fb: FormBuilder,
+    private storage: AngularFireStorage,
+  ) {}
 
   ngOnInit(): void {
     this.initForm({}, false);
@@ -92,5 +78,37 @@ export class CmpPoststep1Component implements OnInit {
       your_email: [ { value: values['your_email'] || this.email, disabled: true }, [ Validators.email, Validators.required ] ],
       owner_addr: [ { value: this.store.curUser, disabled: true } ],
     });
+  }
+
+  uploadFile = (item: any) => {
+    const file = item.file;
+    const fileName = file.name;
+    const filePath = `${file.lastModified}-${fileName}`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          fileRef.getDownloadURL().subscribe(url => {
+            this.fileList = this.fileList.map(item => {
+              if (item.name == fileName){
+                return {...item, url, thumbUrl: url}
+              }
+              return item
+            })
+          })
+        })
+      )
+      .subscribe(snapshot => {
+        if (snapshot.bytesTransferred == snapshot.totalBytes) {
+          this.fileList = this.fileList.map(item => {
+            if (item.name == fileName) {
+              return { ...item, status: 'done', percent: 100 }
+            }
+            return item;
+          })
+        }
+      })
   }
 }
