808533eb24a59d7b7a409da6890ddcad6cc17b4c
thetadotonline <thetadotonline@gmail.com>
2018-07-30 10:23:10 +0800

    feat: scroll top when change route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  onActivate(event) {
+    window.scroll(0,0);
+    //or document.body.scrollTop = 0;
+    //or document.querySelector('body').scrollTo(0,0)
+
+
+    // smooth
+    // let scrollToTop = window.setInterval(() => {
+    //   let pos = window.pageYOffset;
+    //   if (pos > 0) {
+    //       window.scrollTo(0, pos - 20); // how far to scroll on each step
+    //   } else {
+    //       window.clearInterval(scrollToTop);
+    //   }
+    // }, 16);
+
+    // condation
+    // if e.constructor.name
+    
+  }
 }
