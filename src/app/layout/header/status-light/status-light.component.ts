aae9b397b5bf125c258124ec3c419ab8fdd38344
thetadotonline <thetadotonline@gmail.com>
2018-08-22 21:46:57 +0800

    feat: get balance

--- src/app/layout/header/status-light/status-light.component.ts
+++ src/app/layout/header/status-light/status-light.component.ts
@@ -15,7 +15,9 @@ export class StatusLightComponent implements OnInit, AfterViewInit {
   private _web3;
   
   canYaMainContract = '0x1d462414fe14cf489c7a21cac78509f4bf8cd7c0';
-  constructor(private contractService: ContractsService) {
+  constructor(
+    private cs: ContractsService
+  ) {
     if (typeof window.web3 !== 'undefined') {
       // Use Mist/MetaMask's provider
       this._web3 = new Web3(window.web3.currentProvider);
@@ -24,48 +26,15 @@ export class StatusLightComponent implements OnInit, AfterViewInit {
   ngOnInit() {
   }
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
     this.getCurrentNetwork().then(network => {
       this.store.curNet = network;
       console.log(this.store.curNet);
     }); 
-    this.contractService.getAccount().then(user=>{
-      /**
-       *  Get the CAN token amount from etherscan 
-       *  In CANWork, we utilize CANPay to do this,
-       *  however since CANPay has not been integrated to CANSeek,
-       *  this means etherscan's API had to be used.
-       *  
-      */
-      this.store.curUser = user;  //get the current user's address
-      this.store.balance = this.getTokenBalanceAtAddress(this.store.curUser, this.canYaMainContract, 6); 
-    })
+    this.store.curUser = await this.cs.getAccount();
+    this.store.balance = await this.cs.getCANBalance();
   }
-  /**
-   *  A generic code to get token balances.
-   * 
-   */
-  getTokenBalanceAtAddress(targetAddress, tokenAddress, precision){ 
-    let etherscanApi = 'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress='; //the API link.
-    let etherscanApiCanYa = etherscanApi + tokenAddress; //change this value if you want to use other token.
-    let canAtAddress = etherscanApiCanYa + '&address=' + targetAddress + '&tag=latest';  
-    let result = Math.floor(this.getJSON(canAtAddress).result / (Math.pow(10, precision)));  // divide by 10 the power of the token's precision to get the actual, exact number of CAN you own.
-    return result;
-  }
-
-  /** JSON Parser */
-  getJSON(url) {
-    var resp;
-    var xmlHttp;
-    resp = '';
-    xmlHttp = new XMLHttpRequest();
-    if (xmlHttp != null) {
-      xmlHttp.open("GET", url, false);
-      xmlHttp.send(null);
-      resp = xmlHttp.responseText;
-    }
-    return JSON.parse(resp);
-  } 
+  
   /* Get the current network type */ 
   public async getCurrentNetwork() {
     console.log('getting network....');
