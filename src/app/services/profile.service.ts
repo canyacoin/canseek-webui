3872e7faff343fdd6e5ab6b2661405dd1e077a5b
Xinjie-canya <xinjie@canya.com>
2018-09-05 07:44:08 +0800

    feat: update photo

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -69,7 +69,8 @@ export class ProfileService {
         nzOkText: 'OK',
         nzCancelText: 'Resend',
         nzOnCancel: async() => {
-          await this.sendEmail(email, clearEmpty(displayName));
+          await this.updatePhoto(email, clearEmpty(displayName));
+          await this.sendEmail();
         }
       });
       return;
@@ -78,21 +79,27 @@ export class ProfileService {
     try {
       await this.create(email, displayName);
     } catch(err) {
+      this.updatePhoto(email, clearEmpty(displayName));
       await this.login(email);
     }
   }
 
-  async sendEmail(email: string, displayName: string = 'Jane Doe') {
-    const photoURL = await gravatar.url(email);
-
-    await this.afAuth.auth.currentUser.updateProfile({displayName, photoURL});
+  async sendEmail() {
+    
     await this.afAuth.auth.currentUser.sendEmailVerification();
     this.msgModal('success', MsgVerifyEmailSent);
   }
 
+  async updatePhoto(email: string, displayName: string = 'Jane Doe') {
+    const photoURL = await gravatar.url(email);
+
+    await this.afAuth.auth.currentUser.updateProfile({displayName, photoURL});
+  }
+
   async create(email: string, displayName: string) {
     await this.afAuth.auth.createUserWithEmailAndPassword(email, email);
-    await this.sendEmail(email, clearEmpty(displayName));
+    await this.updatePhoto(email, clearEmpty(displayName));
+    await this.sendEmail();
   }
 
   async login(email: string) {
