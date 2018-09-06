1872964094a0068e822fcd0c6ddaa84d3f4f71c0
Xinjie-canya <xinjie@canya.com>
2018-09-06 14:40:19 +0800

    feat: debug deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -2,25 +2,9 @@
 
 # develop
 if [ "$TRAVIS_BRANCH" = "develop" ]; then 
-    ENV=dev npm run build 
-    # zip
-    && ENV=dev npm run zip
-    # clear
-    && ssh -i $encrypted_e7d9ab5b8337_key -o "StrictHostKeyChecking no" root@$HOST 'rm dist-dev.zip' 
-    # upload
-    && scp -i $encrypted_e7d9ab5b8337_key dist-dev.zip root@$HOST:/root
-    # unzip
-    && ssh -i $encrypted_e7d9ab5b8337_key -o "StrictHostKeyChecking no" root@$HOST 'unzip -o dist-dev.zip';
+    ENV=dev npm run build && ENV=dev npm run zip && ssh -i $encrypted_e7d9ab5b8337_key -o "StrictHostKeyChecking no" root@$HOST 'rm dist-dev.zip'  && scp -i $encrypted_e7d9ab5b8337_key dist-dev.zip root@$HOST:/root && ssh -i $encrypted_e7d9ab5b8337_key -o "StrictHostKeyChecking no" root@$HOST 'unzip -o dist-dev.zip';
 fi
 # master
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    ENV=prod npm run build 
-    # zip
-    && ENV=prod npm run zip
-    # clear
-    && ssh -i $encrypted_e7d9ab5b8337_key -o "StrictHostKeyChecking no" root@$HOST 'rm dist-prod.zip' 
-    # upload
-    && scp -i $encrypted_e7d9ab5b8337_key dist-prod.zip root@$HOST:/root
-    # unzip
-    && ssh -i $encrypted_e7d9ab5b8337_key -o "StrictHostKeyChecking no" root@$HOST 'unzip -o dist-prod.zip';
+    ENV=prod npm run build && ENV=prod npm run zip && ssh -i $encrypted_e7d9ab5b8337_key -o "StrictHostKeyChecking no" root@$HOST 'rm dist-prod.zip'  && scp -i $encrypted_e7d9ab5b8337_key dist-prod.zip root@$HOST:/root && ssh -i $encrypted_e7d9ab5b8337_key -o "StrictHostKeyChecking no" root@$HOST 'unzip -o dist-prod.zip';
 fi
